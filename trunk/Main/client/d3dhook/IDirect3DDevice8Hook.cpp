ecd2ccc5df9fb8b45fe91cb1527acfeb544d6a23
jacks.mini.net <jacks.mini.net@45e629aa-34f5-11de-82fb-7f665ef830f7>
2009-05-28 21:34:58 +0000

    Set so when debugging the client it is in window mode. Attempted to fix command line argument problem.

--- trunk/Main/client/d3dhook/IDirect3DDevice8Hook.cpp
+++ trunk/Main/client/d3dhook/IDirect3DDevice8Hook.cpp
@@ -35,14 +35,14 @@ HRESULT __stdcall IDirect3DDevice8Hook::Present(CONST RECT* pSourceRect, CONST R
 
 HRESULT __stdcall IDirect3DDevice8Hook::Reset(D3DPRESENT_PARAMETERS* pPresentationParameters)
 {
-	//if (bWindowedMode)
-	//{
+#if DEBUG
 		pPresentationParameters->Windowed = 1;
 		pPresentationParameters->Flags = 0;
 		pPresentationParameters->FullScreen_RefreshRateInHz = 0;
 		pPresentationParameters->FullScreen_PresentationInterval = 0;
 		SetWindowPos(pPresentationParameters->hDeviceWindow, HWND_NOTOPMOST, 0, 0, pPresentationParameters->BackBufferWidth, pPresentationParameters->BackBufferHeight, SWP_SHOWWINDOW);
-	//}
+		bWindowedMode = TRUE;
+#endif
 
 	if(bbfont)
 	{
